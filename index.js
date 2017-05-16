ba2de288767b8ebd0e3518085802dc69c8e510ea
Eric <VGCbolt@gmail.com>
2017-05-16 17:22:59 -0400

    Bonus Credit: Create at most one global variable

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const personForm = document.querySelector('form')
+const App = {  
+    init() {
+            const personForm = document.querySelector('form')
+            personForm.addEventListener('submit', this.handleSubmit.bind(this))
+        },
+    
+    handleSubmit(ev) {
+        ev.preventDefault()
+        form = ev.target
+        const nameForm = document.querySelector('form')
+        const personName = document.createElement('li')
+        personName.textContent = nameForm.personName.value
 
-const handleSubmit = (ev) => {
-    ev.preventDefault()
-    form = ev.target
-    const personName = document.createElement('li')
-    personName.textContent = personForm.personName.value
+        const details = document.querySelector('.details')
 
-    const details = document.querySelector('.details')
+        details.appendChild(personName)
+    },
+} 
 
-    details.appendChild(personName)
-}
-
-personForm.addEventListener('submit', handleSubmit) 
+App.init()
