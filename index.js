6a0359da95023268cf3c7ef025d1e1eec050902a
Eric <VGCbolt@gmail.com>
2017-05-16 22:14:24 -0400

    Super Mega Bonus Credit Hyper Fighting: Promote names to a blue color

--- index.js
+++ index.js
@@ -3,15 +3,45 @@ const App = {
             const personForm = document.querySelector('form')
             personForm.addEventListener('submit', this.handleSubmit.bind(this))
         },    
+
+    removeButtonCreate() {
+        const button = document.createElement("button")
+        //button.id = nameForm.personName.value
+        button.textContent = "Remove"
+        button.addEventListener('click', this.removeName)
+        return button
+    },
+
+    removeName(ev) { 
+        const name = ev.target.parentNode 
+        const details = document.querySelector('.details')
+        details.removeChild(name)
+    },
+
+    promoteButtonCreate() {
+        const button = document.createElement("button")
+        button.textContent = "Promote"
+        button.addEventListener('click', this.promote)
+        return button
+    },   
+
+    promote(ev) {
+        ev.target.parentNode.style.background = 'blue'
+    }, 
     
     handleSubmit(ev) {
         ev.preventDefault()
         form = ev.target
         const nameForm = document.querySelector('form')
         const personName = document.createElement('li')
-        personName.textContent = nameForm.personName.value
+        personName.textContent = nameForm.personName.value + "    "
+        const removeButton = this.removeButtonCreate(nameForm)
+        personName.appendChild(removeButton)
+        const promoteButton = this.promoteButtonCreate()
+        personName.appendChild(promoteButton)
         const details = document.querySelector('.details')
         details.insertBefore(personName, details.firstChild)
+        //document.getElementById(button.id).addEventListener('click', this.removeName(button.id))
         },
 } 
 
